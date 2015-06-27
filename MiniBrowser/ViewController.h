094e93cc6680ccc1906d467f33665e9b3d061412
Alexey Boyko <dev.aboyko@gmail.com>
2015-06-27 14:45:02 +0700

    Initial Commit

---
+++ MiniBrowser/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MiniBrowser
+//
+//  Created by Alexey Boyko on 27.06.15.
+//  Copyright (c) 2015 Alexey Boyko. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
