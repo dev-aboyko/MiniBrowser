20448e026bc8f13ea99c40d4c39e225aa98588d5
Alexey Boyko <dev.aboyko@gmail.com>
2015-06-27 15:30:09 +0700

    simple browser

--- MiniBrowser/MiniBrowserViewController.h
+++ MiniBrowser/MiniBrowserViewController.h
@@ -1,5 +1,5 @@
 //
-//  ViewController.h
+//  MiniBrowserViewController.h
 //  MiniBrowser
 //
 //  Created by Alexey Boyko on 27.06.15.
@@ -8,8 +8,6 @@
 
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController
-
+@interface MiniBrowserViewController : UIViewController
 
 @end
-
---
+++ MiniBrowser/MiniBrowserViewController.m
@@ -0,0 +1,36 @@
+//
+//  MiniBrowserViewController.m
+//  MiniBrowser
+//
+//  Created by Alexey Boyko on 27.06.15.
+//  Copyright (c) 2015 Alexey Boyko. All rights reserved.
+//
+
+#import "MiniBrowserViewController.h"
+
+@interface MiniBrowserViewController ()
+
+@property (weak, nonatomic) IBOutlet UITextField *urlTextField;
+@property (weak, nonatomic) IBOutlet UIWebView *webView;
+@end
+
+@implementation MiniBrowserViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+- (IBAction)onButtonGo:(id)sender {
+    NSLog(@"Loading %@", _urlTextField.text);
+    NSURL* url = [NSURL URLWithString:_urlTextField.text];
+    NSURLRequest* request = [NSURLRequest requestWithURL:url];
+    [_webView loadRequest:request];
+}
+
+@end
